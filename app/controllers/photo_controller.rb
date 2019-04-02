2a4596c2511671b317d10115b4fad86366a9f9c6
chepyego <jchepyego@gmail.com>
2019-04-03 02:58:15 +0300

    added cloudinary and pusher

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -1,9 +1,22 @@
 class PhotoController < ApplicationController
   def index
-    @post = Post.all
+    @post = Post.all.order("created_at DESC")
   end
 
   def store
-    render plain: params[:caption].inspect
+    #upload image to cloudinary
+    @value = Cloudinary::Uploader.upload(params[:image])    #create a new post object and save to db
+    @post = Post.new({link => @value['secure_url'],:caption => params[:caption]})
+    if @post.save 
+    #trigger an event with pusher
+    #broadcadting posts using pusher
+    Pusher.trigger('posts-channel','new-post',{
+      link:@post.link,
+      caption:@post.caption
+    })
+    # ['...']
+    redirect_to('/')
+    # render plain: params[:caption].inspect
   end
+end
 end
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,7 @@
+require 'pusher'
+Pusher.app_id  = '7507664',
+Pusher.key ='701cae69f1ce45079fbf'
+Pusher.secret =  '5923696b799a2ede3b18',
+Pusher.cluster ='ap2',
+Pusher.logger = Rails.logger,
+Pusher.encrypted = true
